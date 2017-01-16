000611817308cd8be33cf95385fcd4f12b305fc6
Max Goldman <maxg@mit.edu>
2017-01-16 15:01:01 -0500

    Move types and CSV tests to separate files

---
+++ test/omnivore-csv.js
@@ -0,0 +1,116 @@
+'use strict';
+
+const util = require('util');
+
+const should = require('should');
+
+const omnivore = require('../src/omnivore');
+
+describe('Omnivore', function() {
+  
+  let now = new Date();
+  
+  describe('csv', () => {
+    
+    describe('#stringify()', () => {
+      
+      it('should export header row', done => {
+        let sheet = omnivore.csv.stringify([ '/a', '/b' ], [ ], [ 'my comment' ]);
+        sheet.setEncoding('utf-8');
+        sheet.read().should.eql('"username","/a","/b","my comment"\n');
+        done();
+      });
+      it('should export values', done => {
+        let sheet = omnivore.csv.stringify([ '/a', '/b' ], [
+          { username: 'alice', '/a': { value: 5 }, '/b': { value: '\'hello\'\n"there"' } },
+        ]);
+        sheet.setEncoding('utf-8');
+        sheet.read().should.eql('"username","/a","/b"\n"alice",5,"\'hello\'\n""there"""\n');
+        done();
+      });
+    });
+    
+    describe('#parse()', () => {
+      
+      new Map([
+        [ 'boolean',
+          [ [ `true`, true ], [ `"true"`, true ] ] ],
+        [ 'number',
+          [ [ `5`, 5 ], [ `"5"`, 5 ], [ `-5.1`, -5.1 ] ] ],
+        [ 'string',
+          [ [ `a`, 'a' ], [ `"'hello'\n""there"""`, `'hello'\n"there"` ], [ `" "`, ' ' ],
+            [ `null`, 'null' ], [ `undefined`, 'undefined' ] ] ],
+        [ 'undefined',
+          [ [ ``, undefined ], [ `""`, undefined ], [ `   `, undefined ] ] ],
+      ]).forEach((pairs, type) => {
+        describe(type, () => {
+          new Map(pairs).forEach((expected, value) => {
+            it(`should parse value ${util.inspect(value)}`, done => {
+              let sheet = omnivore.csv.parse(`username,/middle,/end
+                                              alice,${value},${value}`);
+              sheet.once('parsed', (keys, rows) => {
+                rows.should.read([ { values: [ expected, expected ] } ]);
+                done();
+              });
+            });
+          });
+        });
+      });
+      
+      it('should parse users and keys', done => {
+        let sheet = omnivore.csv.parse(`username,/a,/b
+                                        alice
+                                        bob,1,2`);
+        sheet.once('parsed', (keys, rows) => {
+          keys.should.eql([ '/a', '/b' ]);
+          rows.should.read([ { username: 'alice' }, { username: 'bob' } ]);
+          done();
+        });
+      });
+      
+      it('should ignore past invalid key', done => {
+        let sheet = omnivore.csv.parse(`username,/a,/b-c,/d/e/f,x,/g,/h
+                                        alice,1,2,3,4,5,6`);
+        sheet.once('parsed', (keys, rows) => {
+          keys.should.eql([ '/a', '/b-c', '/d/e/f' ]);
+          rows.should.read([ { username: 'alice', values: [ 1, 2, 3 ] } ]);
+          done();
+        });
+      });
+      
+      it('should ignore extra values', done => {
+        let sheet = omnivore.csv.parse(`username,/a,/b
+                                        alice,1.1,2.2,3.3`);
+        sheet.once('parsed', (keys, rows) => {
+          keys.should.eql([ '/a', '/b' ]);
+          rows.should.read([ { username: 'alice', values: [ 1.1, 2.2 ] } ]);
+          done();
+        });
+      });
+      
+      it('should fill in missing values', done => {
+        let sheet = omnivore.csv.parse(`username,/a,/b
+                                        alice,apple`);
+        sheet.once('parsed', (keys, rows) => {
+          keys.should.eql([ '/a', '/b' ]);
+          rows.should.read([ { username: 'alice', values: [ 'apple', undefined ] } ]);
+          done();
+        });
+      });
+      
+      it('should include invalid users', done => {
+        let sheet = omnivore.csv.parse(`username,/a
+                                        alice@mit,1
+                                        bob,2`);
+        sheet.once('parsed', (keys, rows) => {
+          keys.should.eql([ '/a' ]);
+          rows.should.read([
+            { username: 'alice@mit', valid: false, values: [ 1 ] },
+            { username: 'bob', valid: true, values: [ 2 ] },
+          ]);
+          done();
+        });
+      });
+    });
+  });
+});
---
+++ test/omnivore-types.js
@@ -0,0 +1,110 @@
+'use strict';
+
+const util = require('util');
+
+const should = require('should');
+
+const omnivore = require('../src/omnivore');
+
+describe('Omnivore', function() {
+  
+  let now = new Date();
+  
+  describe('types', () => {
+    
+    new Map([
+      [ 'value', [
+        [ false, true ],
+        [ 0, true ],
+        [ NaN, true ],
+        [ Infinity, true ],
+        [ '', true ],
+        [ null, false ],
+        [ undefined, false ],
+        [ [ 1, '2' ], true ],
+        [ [ () => {} ], false ],
+        [ { 1: '2', 'three': [ 'four' ], '5': { } }, true ],
+        [ { x: null }, false ],
+        [ { y: new Date(0) }, false ],
+        [ { z: { a: /b/ } }, false ],
+      ] ],
+      [ 'course', [
+        [ '6.005/fa15', true ],
+        [ 'SIX.DOUBLEOHFIVE/fa15', true ],
+        [ '6.S04/fa15', true ],
+        [ '6.s04/fa15', false ],
+      ] ],
+      [ 'key_path', [
+        [ '', false ],
+        [ '/', true ],
+        [ '/a', true ],
+        [ '/a/', false ],
+        [ '/a/b', true ],
+        [ '/*/b', false ],
+        [ '/a/*', false ],
+        [ 'a/b', false ],
+      ] ],
+      [ 'key_path_query', [
+        [ '', true ],
+        [ '/', true ],
+        [ '/a', true ],
+        [ '/a/', false ],
+        [ '/a/b', true ],
+        [ '/*/b', true ],
+        [ '/a/*', true ],
+        [ 'a/b', true ],
+        [ '*/b', true ],
+        [ 'a/*', true ],
+      ] ],
+      [ 'key_ltree_query', [
+        [ '', true ],
+        [ '.', false ],
+        [ 'a', true ],
+        [ 'a.', false ],
+        [ 'a.b', true ],
+        [ '*.b', false ],
+        [ '*{1}.b', true ],
+        [ 'b.*{1}', true ],
+      ] ],
+    ]).forEach((pairs, type) => {
+      describe(type, () => {
+        new Map(pairs).forEach((expected, value) => {
+          it(`${util.inspect(value)} ${expected ? 'is' : 'is not'} ${type}`, () => {
+            omnivore.types.which(value, [ type ]).should.equal(expected ? type : 'none');
+            omnivore.types.is(value, type).should.equal(expected);
+            if (expected) {
+              omnivore.types.assert(value, type).should.eql(value);
+            } else {
+              (function() { omnivore.types.assert(value, type); }).should.throw();
+            }
+          });
+        });
+      });
+    });
+    
+    describe('#common()', () => {
+      
+      it('should return empty for no keys', () => {
+        omnivore.types.common([]).should.eql('');
+      });
+      it('should return single key', () => {
+        omnivore.types.common([ '/a/b/c' ]).should.eql('/a/b/c');
+      });
+      it('should find empty common prefix', () => {
+        omnivore.types.common([ '/a/b/c', '/b/c' ]).should.eql('');
+      });
+      it('should find one-element common prefix', () => {
+        omnivore.types.common([ '/a/b/c', '/a/c/d' ]).should.eql('/a');
+      });
+      it('should find multi-element common prefix', () => {
+        omnivore.types.common([ '/a/b/c', '/a/b/d' ]).should.eql('/a/b');
+      });
+    });
+    
+    describe('#dateTimeString()', () => {
+      it('should format date', () => {
+        omnivore.types.dateTimeString(new Date(2017, 0, 1, 13)).should.eql('Sun Jan 1 1:00p');
+      });
+    });
+  });
+});
