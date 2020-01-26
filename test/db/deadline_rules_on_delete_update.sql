8ea2bea354ff849a91d451d6b60204645f992c8e
Max Goldman <maxg@mit.edu>
2020-01-26 15:36:28 -0500

    Fix comparisons of possibly-null values in some database tests

--- test/db/deadline_rules_on_delete_update.sql
+++ test/db/deadline_rules_on_delete_update.sql
@@ -4,11 +4,11 @@ INSERT INTO penalties VALUES ('unknown', 'Unknown!', 'un => known');
 INSERT INTO deadline_rules VALUES ('test.a*', t(), 'unknown');
 
 SELECT deadline, penalty_id INTO STRICT result FROM keys WHERE key = 'test.alpha';
-PERFORM assert(result.deadline = t(), t()::TEXT || result::TEXT);
-PERFORM assert(result.penalty_id = 'unknown');
+PERFORM assert(result.deadline IS NOT DISTINCT FROM t(), t()::TEXT || result::TEXT);
+PERFORM assert(result.penalty_id IS NOT DISTINCT FROM 'unknown');
 
 DELETE FROM deadline_rules;
 
 SELECT deadline, penalty_id INTO STRICT result FROM keys WHERE key = 'test.alpha';
-PERFORM assert(result.deadline = NULL);
-PERFORM assert(result.penalty_id = NULL);
+PERFORM assert(result.deadline IS NULL);
+PERFORM assert(result.penalty_id IS NULL);
--- test/db/deadline_rules_on_update_update_new.sql
+++ test/db/deadline_rules_on_update_update_new.sql
@@ -5,11 +5,11 @@ RETURNING penalty_id INTO result;
 INSERT INTO deadline_rules VALUES ('test.a*', t(), result.penalty_id);
 
 SELECT deadline, penalty_id INTO STRICT result FROM keys WHERE key = 'test.alpha';
-PERFORM assert(result.deadline = t());
-PERFORM assert(result.penalty_id <> NULL);
+PERFORM assert(result.deadline IS NOT DISTINCT FROM t());
+PERFORM assert(result.penalty_id IS NOT NULL);
 
 UPDATE deadline_rules SET deadline = t_minus('1 hour');
 
 SELECT deadline, penalty_id INTO STRICT result FROM keys WHERE key = 'test.alpha';
-PERFORM assert(result.deadline = t_minus('1 hour'));
-PERFORM assert(result.penalty_id <> NULL);
+PERFORM assert(result.deadline IS NOT DISTINCT FROM t_minus('1 hour'));
+PERFORM assert(result.penalty_id IS NOT NULL);
